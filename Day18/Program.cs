a3fa3209da73e2d1f4836a4059070f3a3a8cfaaa
Rory Collins <rory.collins@hotmail.co.uk>
2023-12-23 17:33:00 +0000

    Failed attempt using flood fill

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Day18;
 
-var input = File.ReadAllLines("sample_input.txt").ToList();
+var input = File.ReadAllLines("input.txt").ToList();
 var solution = new Solution(input);
 
 Console.WriteLine($"Part one: {solution.PartOne()}");
--- Day18/Solution.cs
+++ Day18/Solution.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Text.RegularExpressions;
 using Shared;
 using static Shared.Direction;
@@ -7,12 +8,16 @@ namespace Day18;
 public class Solution
 {
     private readonly IEnumerable<(Direction Direction, int Steps)> commands;
+    private HashSet<Coordinate2D> boundaryCoordinates;
+    private readonly List<Coordinate2D> interiorCoordinates = [];
+
 
     public Solution(List<string> input)
     {
         commands = input
             .Select(line => Regex.Match(line, @"(?<Direction>R|L|U|D) (?<Steps>\d+)"))
-            .Select(m => (Direction:GetDirection(m.Groups["Direction"].Value), Steps:int.Parse(m.Groups["Steps"].Value)));
+            .Select(m => (Direction: GetDirection(m.Groups["Direction"].Value),
+                Steps: int.Parse(m.Groups["Steps"].Value)));
     }
 
     private static Direction GetDirection(string d)
@@ -30,9 +35,107 @@ public class Solution
     public object PartOne()
     {
         var start = Coordinate2D.Origin;
-        var cs = commands.Aggregate(new HashSet<Coordinate2D>{start}, (acc, c) => acc.Concat(ProcessCommand(acc.Last(), c.Direction, c.Steps)).ToHashSet());
+        boundaryCoordinates = commands.Aggregate(new HashSet<Coordinate2D> { start },
+            (acc, c) => acc.Concat(ProcessCommand(acc.Last(), c.Direction, c.Steps)).ToHashSet());
 
-        return cs.Count;
+        var yMax = boundaryCoordinates.Max(it => it.Y);
+        var topLeftInsideBorder = boundaryCoordinates
+            .Where(it => it.Y == yMax - 1)
+            .MinBy(it => it.X)?
+            .Move(East) ?? throw new ProgrammerMistake();
+        
+        Fill(topLeftInsideBorder);
+
+        PrintMap();
+
+        return boundaryCoordinates.Count + interiorCoordinates.Count;
+    }
+
+    private void Fill(Coordinate2D start)
+    {
+        var s = new Queue<(int, int, int, int)>();
+        s.Enqueue((start.X, start.X, start.Y, 1));
+        s.Enqueue((start.X, start.X, start.Y - 1, -1));
+        while (s.Count > 0)
+        {
+            var (x1, x2, y, dy) = s.Dequeue();
+            var x = x1;
+            if (Inside(x, y))
+            {
+                while (Inside(x - 1, y))
+                {
+                    interiorCoordinates.Add(new(x - 1, y));
+                    x = x - 1;
+                }
+
+                if (x < x1)
+                {
+                    s.Enqueue((x, x1 - 1, y - dy, -dy));
+                }
+            }
+
+            while (x1 <= x2)
+            {
+                while (Inside(x1, y))
+                {
+                    interiorCoordinates.Add(new(x1, y));
+                    x1++;
+                }
+
+                if (x1 > x)
+                {
+                    s.Enqueue((x, x1 - 1, y + dy, dy));
+                }
+
+                if (x1 - 1 > x2)
+                {
+                    s.Enqueue((x2 + 1, x1 - 1, y - dy, -dy));
+                }
+
+                x1++;
+                while (x1 < x2 && !Inside(x1, y))
+                {
+                    x1++;
+                }
+
+                x = x1;
+            }
+        }
+    }
+
+    private bool Inside(int x, int y)
+    {
+        return !boundaryCoordinates.Contains(new Coordinate2D(x, y));
+    }
+
+    private void PrintMap()
+    {
+        var xMax = boundaryCoordinates.Max(it => it.X);
+        var xMin = boundaryCoordinates.Min(it => it.X);
+        var yMax = boundaryCoordinates.Max(it => it.Y);
+        var yMin = boundaryCoordinates.Min(it => it.Y);
+
+        for (int y = yMax; y >= yMin; y--)
+        {
+            for (int x = xMin; x <= xMax; x++)
+            {
+                var c = new Coordinate2D(x, y);
+                if (boundaryCoordinates.Contains(c))
+                {
+                    Console.Write('#');
+                    continue;
+                }
+                if (interiorCoordinates.Contains(c))
+                {
+                    Console.Write('X');
+                    continue;
+                }
+
+                Console.Write('.');
+            }
+
+            Console.WriteLine();
+        }
     }
 
     private static List<Coordinate2D> ProcessCommand(Coordinate2D start, Direction direction, int steps)
@@ -42,6 +145,7 @@ public class Solution
         {
             cs.Add(start.Add(Coordinate2D.FromDirection(direction) * i));
         }
+
         return cs;
     }
 
